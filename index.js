2928af98aa326c0c3a5f214a03668890d30c445e
FulgrimThird <163415797+FulgrimThird@users.noreply.github.com>
2024-07-23 07:38:38 +0300

    Update index.js
    ca

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const prodConfig = {
     "postgres://candidate:62I8anq3cFq5GYh2u4Lh@rc1b-r21uoagjy1t7k77h.mdb.yandexcloud.net:6432/db1",
   ssl: {
     rejectUnauthorized: true,
-    ca: fs.readFileSync("/home/runner/.postgresql/root.crt").toString(),
+    ca: fs.readFileSync("CA.pem").toString(),
   },
 };
 
